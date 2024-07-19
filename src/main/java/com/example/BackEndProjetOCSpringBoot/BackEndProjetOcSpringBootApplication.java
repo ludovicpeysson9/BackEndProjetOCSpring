703fe25d16da7be5424801bbfc4fe26e1017b347
ludo <ludovic.peysson@laposte.net>
2024-07-19 14:51:23 +0200

    initial commit

---
+++ src/main/java/com/example/BackEndProjetOCSpringBoot/BackEndProjetOcSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackEndProjetOCSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndProjetOcSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndProjetOcSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BackEndProjetOCSpringBoot/BackEndProjetOcSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackEndProjetOCSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndProjetOcSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
